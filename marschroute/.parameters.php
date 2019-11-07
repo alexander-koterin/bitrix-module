abbfc2cd99b81d1747fda4ca070a065ef1d4c3b7
Alexandr Koterin <alexander.koterin@studio-moderna.com>
2019-11-07 20:46:56 +0300

    Add parameters

---
+++ marschroute/.parameters.php
@@ -0,0 +1,24 @@
+<?
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentParameters = array(
+    "PARAMETERS" => array(
+        "YANDEX_MAPS_KEY" => array(
+            "NAME" => "API-ключ для Яндекс.Карт",
+            "TYPE" => "STRING",
+            "MULTIPLE" => "N",
+            "DEFAULT" => "",
+            "COLS" => 100,
+        )
+    )
+);
+
+
+
+
+
+
+
+
+
